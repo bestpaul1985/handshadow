b8e70b9e6e5cd39c358516c406ebf66a46ef9b3b
Peng Cheng <bestpaul1985@gmail.com>
2013-07-23 23:52:27 -0400

    recode gameplay code

---
+++ handShadow/src/inGameMenu.cpp
@@ -0,0 +1,9 @@
+//
+//  inGameMenu.cpp
+//  handShadow
+//
+//  Created by PengCheng on 7/23/13.
+//
+//
+
+#include "inGameMenu.h"
---
+++ handShadow/src/inGameMenu.h
@@ -0,0 +1,14 @@
+//
+//  inGameMenu.h
+//  handShadow
+//
+//  Created by PengCheng on 7/23/13.
+//
+//
+
+#ifndef __handShadow__inGameMenu__
+#define __handShadow__inGameMenu__
+
+#include <iostream>
+
+#endif /* defined(__handShadow__inGameMenu__) */
