96661b4f9ad0491c9c5834e5900c49ba270485c8
Peng Cheng <bestpaul1985@gmail.com>
2013-07-26 11:46:31 -0400

    add items function

---
+++ handShadow/src/item.cpp
@@ -0,0 +1,9 @@
+//
+//  item.cpp
+//  handShadow
+//
+//  Created by PengCheng on 7/25/13.
+//
+//
+
+#include "item.h"
---
+++ handShadow/src/item.h
@@ -0,0 +1,14 @@
+//
+//  item.h
+//  handShadow
+//
+//  Created by PengCheng on 7/25/13.
+//
+//
+
+#ifndef __handShadow__item__
+#define __handShadow__item__
+
+#include <iostream>
+
+#endif /* defined(__handShadow__item__) */
